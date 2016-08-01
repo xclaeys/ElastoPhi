27c96737a94b0ae64c1393dc7c66529bd56e074a
Pierre Marchand <pierre.jimthefrench@gmail.com>
2016-08-01 14:37:37 +0200

    prog.ccp mis a jour

--- src/prog.cpp
+++ src/prog.cpp
@@ -22,73 +22,34 @@ using namespace std;
 int main(int argc, char* argv[]){
 
   
+	////////////////========================================================////////////////
+	////////////////////////////////========  Input ========////////////////////////////////
+	
+	// Check the number of parameters
+	if (argc < 2) {
+		// Tell the user how to run the program
+		cerr << "Usage: " << argv[0] << " input name" << endl;
+		/* "Usage messages" are a conventional way of telling the user
+		 * how to run a program if they enter the command incorrectly.
+		 */
+		return 1;
+	}
+	
+	// Load the inputs
+	string inputname = argv[1];
+	Param Parametres(inputname);
+ 
+	cout<<"############# Inputs #############"<<endl;
+	cout<<"Eta : "+NbrToStr(Parametres.eta)<<endl;
+	cout<<"Epsilon : "+NbrToStr(Parametres.epsilon)<<endl;
+	cout<<"Data path : "+Parametres.datapath<<endl;
+	cout<<"Output path : "+Parametres.outputpath<<endl;
+	cout<<"Mesh name : "+Parametres.meshname<<endl;
+	cout<<"Matrix name : "+Parametres.matrixname<<endl;
+	cout<<"##################################"<<endl;
+ 
   ////////////////========================================================////////////////
-  ////////////////////////////////========  Input ========////////////////////////////////
-  
-  // Check the number of parameters
-  if (argc < 2) {
-    // Tell the user how to run the program
-    cerr << "Usage: " << argv[0] << " input name" << endl;
-    /* "Usage messages" are a conventional way of telling the user
-     * how to run a program if they enter the command incorrectly.
-     */
-    return 1;
-  }
-  
-  // Load the inputs
- string inputname = argv[1];
- Param Parametres(inputname);
- 
- cout<<"############# Inputs #############"<<endl;
- cout<<"Eta : "+NbrToStr(Parametres.eta)<<endl;
- cout<<"Epsilon : "+NbrToStr(Parametres.epsilon)<<endl;
- cout<<"##################################"<<endl;
- 
-  ////////////////========================================================////////////////
 
-
- // vectReal r;
- // vectR3   x;
- // Matrix   A;
- 
- // LoadMatrix("../data/matrice6.txt",A);
- // LoadPoints("../data/maillage6.txt",x,r);
- ExportGMSH("../data/maillage7.txt");
- 
-
- 
-  
-//  vectReal r;
-//  vectR3   x;
-//  Matrix   A;
-//  
-//  LoadMatrix("../data/matrice6.txt",A);
-//  LoadPoints("../data/maillage6.txt",x,r);
-//  
-//  
-//  for(int j=0; j<r.size(); j++){r[j] = 0.1*r[j];}
-//  HMatrix B(A,x,r,x,r);
-//  
-//  
-//  // Vecteur (pseudo-)aleatoire
-//  int nr  = nb_rows(A);
-//  vectCplx u(nr);
-//  int NbSpl = 1000; 
-//  double du = 5./double(NbSpl);
-//  srand (time(NULL));  
-//  for(int j=0; j<nr; j++){
-//    int n = rand()%(NbSpl+1);
-//    u[j] = n*du;}
-//
-//  vectCplx ua(nr),ub(nr);
-//  MvProd(ua,A,u);
-//  MvProd(ub,B,u);  
-//  Real err = norm(ua-ub)/norm(ua);
-//  cout << "Erreur:\t" << err << endl;
-//  
-//
-//  cout << "Taux de compression:\t";
-//  cout << CompressionRate(B) << endl;  
 	
   
   
