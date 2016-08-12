ca0bba6fc2980e8490cbf8409f4f18ba720e5cf9
Pierre Marchand <pierre.jimthefrench@gmail.com>
2016-08-12 18:03:21 +0200

    Ajout d'un executable pour visualiser la matrix (le précédent était juste une correction)

---
+++ src/visumatrix.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <complex>
+#include <vector>
+#include <cassert>
+#include "point.hpp"
+#include "matrix.hpp"
+#include "lrmat.hpp"
+#include "cluster.hpp"
+#include "hmatrix.hpp"
+#include "loading.hpp"
+#include "export.hpp"
+#include "user.hpp"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+using namespace std;
+
+/**************************************************************************//**
+ * It builds the hierarchical matrix with compressed and dense blocks and
+ * computes the consistency error for the matrix vector product.
+ * It also produces an output file to visualize the compression of the matrix. TO BE WRITTEN
+ * (To be run it requires the input file with the desidered parameters)
+ *****************************************************************************/
+
+int main(int argc, char* argv[]){
+	
+	
+	////////////////========================================================////////////////
+	////////////////////////////////========  Input ========////////////////////////////////
+	
+	// Check the number of parameters
+	if (argc < 2) {
+		// Tell the user how to run the program
+		cerr << "Usage: " << argv[0] << " input name" << endl;
+		/* "Usage messages" are a conventional way of telling the user
+		 * how to run a program if they enter the command incorrectly.
+		 */
+		return 1;
+	}
+	
+	// Load the inputs
+	string inputname = argv[1];
+	Param Parametres(inputname);
+ 
+	cout<<"############# Inputs #############"<<endl;
+	cout<<"Eta : "+NbrToStr(Parametres.eta)<<endl;
+	cout<<"Epsilon : "+NbrToStr(Parametres.epsilon)<<endl;
+	cout<<"Data path : "+Parametres.datapath<<endl;
+	cout<<"Output path : "+Parametres.outputpath<<endl;
+	cout<<"Mesh name : "+Parametres.meshname<<endl;
+	cout<<"Matrix name : "+Parametres.matrixname<<endl;
+	cout<<"##################################"<<endl;
+ 
+	//////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////    Build Hmatrix 	//////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	vectReal r;
+	vectR3   x;
+	Matrix   A;
+	tic();
+	LoadMatrix((Parametres.datapath+"/"+Parametres.matrixname).c_str(),A);
+	LoadPoints((Parametres.datapath+"/"+Parametres.meshname).c_str(),x,r);
+	vectInt tab(nb_rows(A));
+	for (int j=0;j<x.size();j++){
+		tab[3*j]  = j;
+		tab[3*j+1]= j;
+		tab[3*j+2]= j;
+	}
+	toc();
+	tic();
+	HMatrix B(A,x,r,tab);
+	toc();
+	tic();
+	//////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////    Create Output 	//////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////
+	Output(B, "output_local_comp_"+NbrToStr(Parametres.eta)+"_"+NbrToStr(Parametres.epsilon)+"_"+Parametres.matrixname); // to visualize the compression of the matrix
+	toc();
+	
+}
+
